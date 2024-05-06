d0ce5e8e6250ec7481a625ac87ad2d8e3b011c01
RavenColEvol <ravilamkoti@gmail.com>
2024-05-07 00:05:39 +0530

    fix: number precision of rating

---
+++ src/components/DotLoader.tsx
@@ -0,0 +1,13 @@
+import './style.css';
+
+function DotLoader() {
+  return (
+    <div className="spinner">
+      <span className="bounce1"></span>
+      <span className="bounce2"></span>
+      <span className="bounce3"></span>
+    </div>
+  )
+}
+
+export default DotLoader
--- src/components/SidebarItem.tsx
+++ src/components/SidebarItem.tsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { IMAGE_BASE_URL, MINI_SIZE } from "../config";
 import { IMovie } from "./MovieDetail";
 import { categoryOpts } from "./api/options";
+import DotLoader from "./DotLoader";
 
 interface Props {
   category: string;
@@ -17,22 +18,20 @@ export default function SideBarItem({ category, title, open: isOpen }: Props) {
 
   const { data, isLoading } = useQuery(categoryOpts(category));
 
-  return isLoading ? (
-    <p>Loading</p>
-  ) : (
+  return (
     <>
       <p
         className="text-white font-bold py-4 px-4 text-xs tracking-wide uppercase cursor-pointer duration-100"
         onClick={handleToggle}
       >
-        {title}
+        {title} {isLoading ? <DotLoader /> : null}
       </p>
       <div
         className={` ${
           open ? "block" : "hidden"
         } transform bg-gray-800 px-4 py-4 rounded-xl font-semibold text-sm duration-100`}
       >
-        {data.map((movie: IMovie) => (
+        {data?.map((movie: IMovie) => (
           <Link key={movie.id} to={`/${movie.id}`}>
             <div className=" flex mb-4" style={{ cursor: "pointer" }}>
               <div className="overflow-hidden w-12 h-12 bg-cover rounded-lg w-2/12">
@@ -49,7 +48,7 @@ export default function SideBarItem({ category, title, open: isOpen }: Props) {
                     {movie.original_language}
                   </span>
                 </div>
-                <p className="text-yellow-400">{movie.vote_average}</p>
+                <p className="text-yellow-400">{movie.vote_average.toPrecision(2)}</p>
               </div>
             </div>
           </Link>
